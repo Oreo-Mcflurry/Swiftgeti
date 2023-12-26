d08ebe5f14a2128546af4f3a9f7f81359157e97e
유인호 <yuiop1029@naver.com>
2023-12-26 11:20:13 +0900

    Initial Commit

---
+++ Swiftgeti/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftgeti
+//
+//  Created by A_Mcflurry on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftgeti/SwiftgetiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftgetiApp.swift
+//  Swiftgeti
+//
+//  Created by A_Mcflurry on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftgetiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
